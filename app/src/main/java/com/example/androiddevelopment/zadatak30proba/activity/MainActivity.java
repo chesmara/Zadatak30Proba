92ad1374a638494d695995147ed78996f38450a3
Srki <srdjan@ekoneimar.com>
2017-03-10 20:54:39 +0100

    treci komit

--- app/src/main/java/com/example/androiddevelopment/zadatak30proba/activity/MainActivity.java
+++ app/src/main/java/com/example/androiddevelopment/zadatak30proba/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.androiddevelopment.zadatak30proba.activity;
 
+import android.app.Dialog;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
@@ -10,25 +11,34 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.androiddevelopment.zadatak30proba.R;
 import com.example.androiddevelopment.zadatak30proba.db1.DatabaseHelper;
+import com.example.androiddevelopment.zadatak30proba.db1.model.Glumac;
+import com.j256.ormlite.android.apptools.OpenHelperManager;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        refresh();
 
     }
 
@@ -42,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_create:
-                //addItem();
+                addItem();
                 Toast.makeText(this, "Opcija unos novog glumca pokrenuta", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.action_update:
@@ -55,6 +65,99 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void addItem( )  {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_layout);
+
+        final EditText glumacName = (EditText) dialog.findViewById(R.id.glumac_name);
+        final EditText glumacDescr = (EditText) dialog.findViewById(R.id.glumac_description);
+        final EditText glumacRating = (EditText) dialog.findViewById(R.id.glumac_rating);
+        final EditText glumacBirthDay = (EditText) dialog.findViewById(R.id.glumac_birth);
+
+
+        Button ok = (Button) dialog.findViewById(R.id.ok);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name= glumacName.getText().toString();
+                String desct= glumacDescr.getText().toString();
+                String rating=glumacRating.getText().toString();
+                String birthday=glumacBirthDay.getText().toString();
+
+                Toast.makeText(MainActivity.this,"pre ok", Toast.LENGTH_SHORT).show();
+
+                Glumac glumac = new Glumac();
+                glumac.setName(name);
+                glumac.setDescribe(desct);
+                glumac.setRating(rating);
+                glumac.setBirthday(birthday);
+
+                Toast.makeText(MainActivity.this,"posle", Toast.LENGTH_SHORT).show();
+
+                try {
+                    Toast.makeText(MainActivity.this,"Glumac pre inserted!", Toast.LENGTH_SHORT).show();
+
+                    getDatabaseHelper().getGlumacDao().create(glumac);
+                    refresh();
+                    Toast.makeText(MainActivity.this,"Glumac inserted!", Toast.LENGTH_SHORT).show();
+
+                    List<Glumac> list = getDatabaseHelper().getGlumacDao().queryForAll();
+                    Toast.makeText(MainActivity.this, String.valueOf(list.size()) ,Toast.LENGTH_SHORT).show();
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+
+
+        Button cancel = (Button) dialog.findViewById(R.id.cancel);
+        cancel.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+
+    }
+
+    public DatabaseHelper getDatabaseHelper() {
+        if (databaseHelper == null) {
+            databaseHelper = OpenHelperManager.getHelper(this, DatabaseHelper.class);
+        }
+        return databaseHelper;
+    }
+
+
+    private void refresh() {
+        ListView listview = (ListView) findViewById(R.id.listaGlumaca);
+
+        if (listview != null){
+            ArrayAdapter<Glumac> adapter = (ArrayAdapter<Glumac>) listview.getAdapter();
+
+            if(adapter!= null)
+            {
+                try {
+                    adapter.clear();
+                    List<Glumac> list = getDatabaseHelper().getGlumacDao().queryForAll();
+                    Toast.makeText(MainActivity.this, String.valueOf(list.size()) ,Toast.LENGTH_SHORT).show();
+                    adapter.addAll(list);
+
+                    adapter.notifyDataSetChanged();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+
     }
 
 
---
+++ app/src/main/java/com/example/androiddevelopment/zadatak30proba/db1/model/Glumac.java
@@ -0,0 +1,110 @@
+package com.example.androiddevelopment.zadatak30proba.db1.model;
+
+import com.j256.ormlite.dao.ForeignCollection;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.field.ForeignCollectionField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * Created by SNinkovic_ns on 4.3.2017.
+ */
+@DatabaseTable(tableName = Glumac.TABLE_NAME_GLUMAC)
+public class Glumac {
+
+    public static final String TABLE_NAME_GLUMAC = "glumci";
+
+    public static final String FIELD_NAME_ID  =  "id";
+    public static final String FIELD_NAME_NAME = "name";
+    public static final String FIELD_NAME_DESCRIBE = "describe";
+    public static final String FIELD_NAME_RATING = "rating";
+    public static final String FIELD_NAME_BIRTHDAY ="birthday";
+    public static final String FIELD_NAME_IMAGE = "image";
+    public static final String FIELD_NAME_FILMOVI = "filmovi";
+
+
+    @DatabaseField(columnName = FIELD_NAME_ID, generatedId = true)
+    private int id;
+
+    @DatabaseField(columnName = FIELD_NAME_NAME)
+    private String name;
+
+    @DatabaseField(columnName = FIELD_NAME_DESCRIBE)
+    private String describe;
+
+    @DatabaseField(columnName = FIELD_NAME_RATING)
+    private String rating;
+
+    @DatabaseField(columnName = FIELD_NAME_BIRTHDAY)
+    private String birthday;
+
+  /*  @DatabaseField(columnName = FIELD_NAME_IMAGE)
+    private String image;
+
+*/
+    @ForeignCollectionField(foreignFieldName = "filmovi", eager=true)
+    private ForeignCollection<Filmovi> filmovi;
+
+
+    public Glumac(){
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+   /* public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+*/
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
